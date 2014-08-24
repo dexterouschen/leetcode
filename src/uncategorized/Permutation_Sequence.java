87748ac805434fa59eb6992bb943a74a369a2cfc
Jerry <chem.dexter@gmail.com>
2014-08-23 22:32:43 -0400

    108 problems revised.

--- src/uncategorized/Permutation_Sequence.java
+++ src/uncategorized/Permutation_Sequence.java
@@ -21,42 +21,57 @@ import java.util.*;
 
 public class Permutation_Sequence {
 
-	/*
-	 * Solve this problem by using induction method. Otherwise you may end up
-	 * with a O(n!) time/space complexity.
+	/**
+	 * This problem can be solved by a deep-search algorithm with a time
+	 * complexity of O(n!). A better solution can be reached iteratively as
+	 * following: let the final answer of (n, k) be a number with each digit as:
+	 * a1a2a3a4...an. What's the number that should be picked at a1 position?
+	 * Think this way: if a1 is picked, then there are (n-1)! different
+	 * combinations of a2 to an. According to the question, k must be a number
+	 * between 0 and (n-1)! (you may prove this by taking a contradiction).
+	 * Hence, the number at a1 position should be equal to the "k/(n-1)!"th
+	 * number in array {1, 2, 3 ... n}. Then what should be picked at the second
+	 * position a2? Let k2 = k % (n-1)!, it should be the "k2/(n-2)!"th number
+	 * in array {1, 2, 3 ... n} (without the number picked at a1 position). Keep
+	 * doing this operation until the remain of kx/(n-x)! == 0, in which case
+	 * the remain of array is taken to fill up ax to an positions.
 	 */
-	// 1st pick of number: index = k/(n-1)!, remain = k%(n-1)!
-	// select the element at the index of the list of remaining numbers;
-	// 2nd pick of number: index = remain/(n-2)!, remain = remain%(n-2)!
-	// select the element at the index of the list of remaining numbers
-	// ... 'i'th pick of number: index = (previous pick's remain)/(n-i)!
-	// select the element at the index of the list of remaining numbers
-	// ... stop when i == n
 	public static String getPermutation(int n, int k) {
-		int index = 1, remain = k - 1;
-		StringBuffer sb = new StringBuffer();
-		ArrayList<Integer> unUsedNums = new ArrayList<>();
-		for (int i = 0; i < n; i++) {
-			unUsedNums.add(i + 1);
+		if (n == 0 || k == 0 || k > factorial(n)) {
+			return null;
 		}
-		while (index <= n) {
-			int curTake = remain / getFactorial(n - index);
-			sb.append(unUsedNums.get(curTake));
-			unUsedNums.remove(curTake);
-			remain = remain % getFactorial(n - index);
-			index++;
+		StringBuilder sb = new StringBuilder();
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 1; i <= n; i++) {
+			list.add(i);
+		}
+		int remain = k - 1, count = 1; // remain = k - 1
+		while (remain != 0 && count <= n) {
+			int take = remain / factorial(n - count);
+			remain = remain % factorial(n - count);
+			sb.append(list.get(take));
+			list.remove(take);
+			count++;
+		}
+		if (!list.isEmpty()) { // if there are remaining elements in array
+			for (int x : list) {
+				sb.append(x);
+			}
 		}
 		return sb.toString();
 	}
 
-	public static int getFactorial(int x) {
-		int factValue = 1;
-		for (int i = 2; i <= x; i++) {
-			factValue = factValue * i;
+	public static int factorial(int n) {
+		int ans = 1;
+		for (int i = n; i > 1; i--) {
+			ans = ans * i;
 		}
-		return factValue;
+		return ans;
 	}
 
+	/**
+	 * Deep search approach, O(n!) complexity. You may not want to use it.
+	 */
 	public static String getPermutationRecursion(int n, int k) {
 		ArrayList<ArrayList<Integer>> lists = new ArrayList<>();
 		ArrayList<Integer> list = new ArrayList<>();
